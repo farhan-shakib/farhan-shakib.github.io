39e7ade6114a8bcaf323e3170ac793ae0410501a
Md. Farhan Shakib <f.shakibcse16@gmail.com>
2024-07-15 12:38:24 +0600

    pelicon theme flex

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,4 +32,16 @@ SOCIAL = (
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+# Flex theme settings
+# THEME = 'themes/pelican-themes/Flex'
+# SITESUBTITLE = 'A Subtitle for My Site'
+# SITETAGLINE = 'A Tagline for My Site'
+# SITELOGO = '/images/logo.png'
+# # FAVICON = '/images/favicon.ico'
+# BROWSER_COLOR = '#333'
+# THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+# DISPLAY_CATEGORIES_ON_MENU = False
+# MENUITEMS = [('Home', '/index.html'),
+#              ('About', '/pages/about.html')]
